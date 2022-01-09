cbbee30e2bdec5faca57a37791af4d4bd53a69c2
kttolik <kttolikdreamer@gmail.com>
2022-01-09 15:30:49 +0200

    update for GN and HW05

--- GessNum02/GNjs.js
+++ GessNum02/GNjs.js
@@ -1,66 +1,70 @@
-
-alert('Привет, я загадал число от min до max вашего диапазона. Попробуй угадать его за x попыток!');
-
-
-
-let btn_check = document.getElementById("check");
-let btn_hellLevell = document.getElementById("change");
+const btn_check = document.getElementById("check");
+const btn_hellLevell = document.getElementById("change");
 btn_hellLevell.onclick = hellLevel;
 
-let guess_box = document.getElementById("guess-box");
-let all_guesses = document.getElementById("all-guesses");
-let hotCold = document.getElementById("hotCold");
-let loose = document.getElementById("loose");
+const guess_box = document.getElementById("guess-box");
+const all_guesses = document.getElementById("all-guesses");
+const hotCold = document.getElementById("hotCold");
+const loose = document.getElementById("loose");
 
-let random_num = Math.floor(Math.random() * 100) + 1;
+let minVal = 0;
+let maxVal = 100;
 
+document.getElementById('min-text').innerHTML = `${minVal}`;
+document.getElementById('max-text').innerHTML = `${maxVal}`;
+// document.getElementById('count-guess-text').innerHTML = `${maxVal}`;
+
+let random_num = Math.round(Math.random() * (maxVal -  minVal + 1)) + 1;
+console.log(random_num);
 let count_guess = 1;
-
-function hellLevel() {
-    random_num = Math.floor(Math.random() * 200) + 1;
-    count_guess = 15;
-}
+let guess = 5;
 
 function checkGuess() {
     let your_guess = Number(guess_box.value);
-    if (your_guess == Number || your_guess === ',' || your_guess === '!' || your_guess === ':'
-        || your_guess === '-' || your_guess === '?' ||  your_guess === '.' ||  your_guess === ' ' ) {
-        alert('Неверное значение!');
-    }
-    if (count_guess <= 4) {
-        if (your_guess < random_num) {
+
+    if (your_guess > maxVal || your_guess < minVal ) {
+        alert('Число вышло за диапазон видимости волшебного шара! Это неугодное число! Попробуй другое!!');
+    } else {
+
+        if (count_guess < guess) {
+            if (your_guess < random_num) {
+                all_guesses.textContent += your_guess + " ";
+                hotCold.textContent = `Не угадал, холоднее... Осталось ${guess - count_guess} попыток`;
+                hotCold.classList.add("wrong");
+                count_guess++;
+                guess_box.value = '';
+            } else if (your_guess > random_num) {
+                all_guesses.textContent += your_guess + " ";
+                hotCold.textContent = `Не угадал, но теплее!!! Осталось ${guess -count_guess} попыток`;
+                hotCold.classList.add("wrong");
+                count_guess++;
+                guess_box.value = '';
+            } else {
+                all_guesses.textContent += your_guess + " ";
+                hotCold.textContent = `Поздравляю! Ты угадал задуманное число за  ${count_guess} попыток`;
+                hotCold.classList.add("success");
+                guess_box.value = '';
+                gameOver();
+            }
+        } else {
             all_guesses.textContent += your_guess + " ";
-            hotCold.textContent = `Не угадал, холоднее... Осталось ${count_guess} попыток`;
+            loose.textContent = `Ты не угадал и был пожран древним богом!`;
             hotCold.classList.add("wrong");
-            count_guess++;
-            guess_box.value = '';
-        }
-        else if (your_guess > random_num) {
-            all_guesses.textContent += your_guess + " ";
-            hotCold.textContent = `Не угадал, но теплее!!! Осталось ${count_guess} попыток`;
-            hotCold.classList.add("wrong");
-            count_guess++;
-            guess_box.value = '';
-        }
-        else {
-            all_guesses.textContent += your_guess + " ";
-            hotCold.textContent = `Поздравляю! Ты угадал задуманное число за  ${count_guess} попыток`;
-            hotCold.classList.add("success");
             guess_box.value = '';
             gameOver();
         }
     }
-    else {
-        all_guesses.textContent += your_guess + " ";
-        loose.textContent = `Ты не угадал и был пожран древним богом!`;
-        hotCold.classList.add("wrong");
-        guess_box.value = '';
-        gameOver();
-        }
 }
+
 
 function gameOver() {
     btn_check.disabled = true;
     guess_box.disabled = true;
 }
 
+function hellLevel() {
+    alert('Адский уровень активирован БА-ХА-ХА! Молитесь смертные!')
+    maxVal = 200;
+    guess = 15;
+
+}
--- Homework 06/src/Homework_05.js
+++ Homework 06/src/Homework_05.js
@@ -3,26 +3,58 @@
 //     Правила:
 // -Яйца варить 5 минут
 // -Вместительность емкости не более 5 яиц одновременно
+/*
 function getCookingTime (eggsAmount) {
     let result;
-//Your code here
+    if (typeof eggsAmount !== 'number' || eggsAmount < 0 || eggsAmount === '') {
+        return false;
+    }
+    if (eggsAmount === 0) {
+        return result = 0;
+    } else {
+    result = Math.ceil(eggsAmount / 5) * 5;
+}
     return result;
 }
 getCookingTime(0); //returns 0
 getCookingTime(5); //returns 5
 getCookingTime(9); //returns 10 (because capacity is 5 so we need to do it 2
 times)
+*/
 
 
 // 2.Получая массив чисел. Все они либо нечетные, либо четные, кроме
 // одного. Тебе нужно его найти.
-    function getNumber (array) {
-    let result ;
-//Your code here
-    return result;
+
+/*
+function getNumber(array) {
+
+    let newArray = [];
+    let oneMoreArray = [];
+
+    if (typeof array !== 'object' || array == 0 || array == '') {
+        return false;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            newArray.push(array[i]);
+        } else {
+            oneMoreArray.push(array[i]);
+        }
+    }
+    if (newArray.length < oneMoreArray.length) {
+        return newArray[0];
+    } else {
+        return oneMoreArray[0];
+    }
 }
+
+
 getNumber([1, 5, 7, 9, 15, 19, 777, -15, -11, 4, 9, 23, -17]) //returns 4
 getNumber([0, 2, 8, -4, 0, -122, 13, -4, 28, 12]) //returns 13
+
+*/
 
 
 
@@ -31,6 +63,7 @@ getNumber([0, 2, 8, -4, 0, -122, 13, -4, 28, 12]) //returns 13
 // фильтрует массив, заданный как первый параметр, и возвращает массив
 // объектов, которые содержат в своих заголовках заданную строку в
 // качестве второго параметра (без учета регистра).
+/*
 function findTitle(array, ‘string’) {
     return [{title has this substring}]
 }
@@ -47,12 +80,13 @@ let arr = [{
     title: 'Js - is the best!'
 }];
 findTitle(arr, 'js'); // return [{ title: 'I like JS'}, { title: 'Js - is the best!' }]
+*/
 
 
 // 4. Принимая строку, ваша функция должна вернуть обьект, в котором
 // ключи – символы строки, значение – количество повторений символов в
 // строке
-function countCharacters(string) {
+/*function countCharacters(string) {
     let result;
 //Your code here
     return result;
@@ -60,12 +94,15 @@ function countCharacters(string) {
 countCharacters(‘sparrow’) // should return {s: 1, p: 1, a: 1, r: 2, o: 1, w: 1}
 countCharacters(‘aabcddeffge’) // should return {a: 2, b: 1, c: 1, d: 2, e: 2, f: 2,
 g: 1}
-countCharacters(‘a 2ab !d’) // should return {a: 2, b:1, d:1, 2:1}
+countCharacters(‘a 2ab !d’) // should return {a: 2, b:1, d:1, 2:1}*/
+
+
 // 5. Принимая число, ваша функция должна найти следующий
 // положительный палиндром большего размера.
 //     Палиндром - это слово, фраза, число или другая последовательность
 // символов, которая читается так же, как вперед и назад, например,
 // «мадам».
+/*
 function getNextPalindrome(number) {
     let result;
 //Your code here
@@ -75,4 +112,4 @@ getNextPalindrome(7) // returns 11
 getNextPalindrome(99) //returns 101
 getNextPalindrome(132) // returns 141
 getNextPalindrome(888) // returns 898
-getNextPalindrome(999) // returns 1001
+getNextPalindrome(999) // returns 1001*/
